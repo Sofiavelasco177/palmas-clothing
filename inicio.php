313abad73fb5cec830033381e73704e77b3e3428
Sofiavelasco177 <velascosantamariaa1105546@gmail.com>
2025-06-05 10:57:47 -0500

    cambios

--- inicio.php
+++ inicio.php
@@ -15,6 +15,8 @@ echo "Bienvenido, " . $_SESSION['username'] . "!";
     <title>Palmas Clothing - Inicio</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    
 
 </head>
 <body>
@@ -30,10 +32,10 @@ echo "Bienvenido, " . $_SESSION['username'] . "!";
                     <li class="nav-item"><a class="nav-link" href="inicio.php">Inicio</a></li>
                     <li class="nav-item"><a class="nav-link" href="productos.html">Productos</a></li>
                     <li class="nav-item"><a class="nav-link" href="contacto.html">Contacto</a></li>
-                    <li class="nav-item"><a class="nav-link" href="nosotros.html">Sobre Nosotros</a></li>
-                    <li class="nav-item"><a class="nav-link" href="carrito.html">Carrito</a></li>
-                    <li class="nav-item"><a class="nav-link active" href="login.php">Login </a></li>
-                    <li class="nav-item"><a class="nav-link active" href="registro.php">Registro</a></li>
+                    <li class="nav-item"><a class="nav-link" href="nosotros.html"> Nosotros</a></li>
+                    <li class="nav-item"><a class="nav-link" href="carrito.html"><i class="bi bi-cart3"></i></a></li>
+                    <li class="nav-item"><a class="nav-link active" href="Login.html"><i class="bi bi-person"></i></a></li>
+                    <li class="nav-item"><a class="nav-link active" href="registro.php"><i class="bi bi-person-plus"></i></a></li>
                     <a class="whatsapp" href="https://wa.me/573232497247" target="_blank" title="Chatea con nosotros" img src="112.jpg" alt="WhatsApp">
                         <img src="wa.jpg" alt="WhatsApp" width="40" height="40">
 
@@ -79,62 +81,36 @@ echo "Bienvenido, " . $_SESSION['username'] . "!";
 
     </div>
 
-    <footer>
-    <div class="izquierda">
-        <h5 id="modal-product-title">Información de Contacto</h5>
-        <i class="bi bi-geo-alt-fill">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
-                <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"/>
-            </svg>Centro Comercial Velez Plaza Local 104 Velez-Santander.</i><br>
-        <i class="bi bi-envelope-plus-fill">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-plus-fill" viewBox="0 0 16 16">
-                <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.026A2 2 0 0 0 2 14h6.256A4.5 4.5 0 0 1 8 12.5a4.49 4.49 0 0 1 1.606-3.446l-.367-.225L8 9.586zM16 4.697v4.974A4.5 4.5 0 0 0 12.5 8a4.5 4.5 0 0 0-1.965.45l-.338-.207z"/>
-                <path d="M16 12.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0m-3.5-2a.5.5 0 0 0-.5.5v1h-1a.5.5 0 0 0 0 1h1v1a.5.5 0 0 0 1 0v-1h1a.5.5 0 0 0 0-1h-1v-1a.5.5 0 0 0-.5-.5"/></svg>Palmas.clothing24cimitarra@gmail.com</i>
-                
-    </div>
-
-    <div class="centro">
-        <h5 id="modal-product-title">PALMAS CLOTHING</h5>
-        <p class="card-description">DEL BARRIO PARA LA FAMA.</p>
-        <h5 id="modal-product-title">Redes Sociales</h5>
-        <div class="social-icons">
-            <i class="bi bi-facebook">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
-                    <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
-                </svg>
-            </i>
-            <i class="bi bi-twitter">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
-                    <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334q.002-.211-.006-.422A6.7 6.7 0 0 0 16 3.542a6.7 6.7 0 0 1-1.889.518 3.3 3.3 0 0 0 1.447-1.817 6.5 6.5 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.32 9.32 0 0 1-6.767-3.429 3.29 3.29 0 0 0 1.018 4.382A3.3 3.3 0 0 1 .64 6.575v.045a3.29 3.29 0 0 0 2.632 3.218 3.2 3.2 0 0 1-.865.115 3 3 0 0 1-.614-.057 3.28 3.28 0 0 0 3.067 2.277A6.6 6.6 0 0 1 .78 13.58a6 6 0 0 1-.78-.045A9.34 9.34 0 0 0 5.026 15"/>
-                </svg>
-            </i>
-            <i class="bi bi-youtube">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-youtube" viewBox="0 0 16 16">
-                    <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.01 2.01 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.01 2.01 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31 31 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.01 2.01 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A100 100 0 0 1 7.858 2zM6.4 5.209v4.818l4.157-2.408z"/>
-                </svg>
-            </div>
-            </i>
+        <footer>
+        <div class="izquierda">
+            <h5>Información de Contacto</h5>
+            <p><i class="bi bi-geo-alt-fill"></i> Centro Comercial Velez Plaza Local 104 Velez-Santander.</p>
+            <p><i class="bi bi-envelope-plus-fill"></i> Palmas.clothing24cimitarra@gmail.com</p>
         </div>
-    </div>
-</div>
-</div>
 
-<div class="derecha">
-<h5 id="modal-product-title">Horario de Atencion.</h5>  
-<p class="card-description">Lunes - Domingo</p>
-<p class="card-description">9am a 12 pm - 2pm a 8pm</p>
-<hr>
+        <div class="centro">
+            <h5>PALMAS CLOTHING</h5>
+            <p class="card-description">DEL BARRIO PARA LA FAMA.</p>
+            <h5>Redes Sociales</h5>
+            <div class="social-icons">
+                <a href="#"><i class="bi bi-facebook"></i></a>
+                <a href="#"><i class="bi bi-twitter"></i></a>
+                <a href="#"><i class="bi bi-youtube"></i></a>
+            </div>
+        </div>
 
-</div>
-</div>
+        <div class="derecha">
+            <h5>Horario de Atención</h5>  
+            <p class="card-description">Lunes - Domingo</p>
+            <p class="card-description">9am a 12 pm - 2pm a 8pm</p>
+        </div>
 
-
-<div class="footer-content">
-<div class="centro">
-    <p>&copy; Derechos Reservados </p>
-    
-</footer>
-
+        <div class="footer-content">
+            <div class="centro">
+                <p>&copy; Derechos Reservados</p>
+            </div>
+        </div>
+    </footer>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
